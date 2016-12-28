a81ad11a285e2d24e3fb6ff0381c5989544043dc
AngryAenghus <conor.roberts92@gmail.com>
2016-12-27 20:55:50 -0500

    Update role.carrier.js

--- src/role.carrier.js
+++ src/role.carrier.js
@@ -1,1 +1,19 @@
+var roleCarrier = {
 
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if(creep.carry.energy < creep.carryCapacity) {
+            var energySources = creep.room.find(RESOURCE_ENERGY);
+            if(creep.pickup(energySources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(energySources[0]);
+            }
+        }
+        else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
+            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns['Spawn1']);
+            }
+        }
+    }
+};
+
+module.exports = roleHarvester;
